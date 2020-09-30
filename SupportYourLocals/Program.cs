feab28d5a1b2a0abc707f611e56bd17197791bff
Neruks <nerijus.puras@mif.stud.vu.lt>
2020-09-30 22:31:59 +0300

    Importing project

---
+++ SupportYourLocals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SupportYourLocals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
