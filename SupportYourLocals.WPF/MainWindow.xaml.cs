2d837a82866d4a117691c9641f008376526b1ef6
Neruks <nerijus.puras@mif.stud.vu.lt>
2020-10-12 12:08:09 +0300

    UI changes. Marketplace search and local sellers search is now on one tab

--- SupportYourLocals.WPF/MainWindow.xaml.cs
+++ SupportYourLocals.WPF/MainWindow.xaml.cs
@@ -25,5 +25,24 @@ namespace SupportYourLocals.WPF
             InitializeComponent();
         }
 
+        private void SearchMarketplacesButton_Click(object sender, RoutedEventArgs e)
+        {
+            GridSellersSearch.Visibility = Visibility.Collapsed;
+            GridMarketplacesSearch.Visibility = Visibility.Visible;
+            SearchSellersButton.FontWeight = FontWeights.Normal;
+            SearchMarketplacesButton.FontWeight = FontWeights.Bold;
+            SearchSellersButton.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#808080"));
+            SearchMarketplacesButton.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void SearchSellersButton_Click(object sender, RoutedEventArgs e)
+        {
+            GridSellersSearch.Visibility = Visibility.Visible;
+            GridMarketplacesSearch.Visibility = Visibility.Collapsed;
+            SearchMarketplacesButton.FontWeight = FontWeights.Normal;
+            SearchSellersButton.FontWeight = FontWeights.Bold;
+            SearchMarketplacesButton.Foreground =  new SolidColorBrush((Color)ColorConverter.ConvertFromString("#808080"));
+            SearchSellersButton.Foreground = new SolidColorBrush(Colors.Black);
+        }
     }
 }
