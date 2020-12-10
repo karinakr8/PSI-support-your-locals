d5fc3e66bcc5c122c0959d1f92991b5528d7ac48
Uxeron <vgabrielius123@gmail.com>
2020-12-10 23:08:40 +0200

    Created Generic Data class with ID and Name, added 0 parameter constructors.

---
+++ SupportYourLocals.Data/DataStructures/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SupportYourLocals.ExtensionMethods;
+
+namespace SupportYourLocals.Data
+{
+    public class GenericData : IComparable<GenericData>
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+        public GenericData() { }
+
+        public GenericData(string name, string id = null)
+        {
+            Name = name;
+            ID = id ?? GenerateId;
+        }
+
+        private static string GenerateId => Guid.NewGuid().ToString("N");
+
+        public int CompareTo(GenericData obj)
+        {
+            return Name.Compare(obj.Name);
+        }
+    }
+}
